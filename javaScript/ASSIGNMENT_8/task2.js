5c77d2c132aa474e83aea8c7dddcd9494a9e779a
JagadeeshChakravarthi <110399596+JagadeeshChakravarthi@users.noreply.github.com>
2023-06-07 09:53:08 +0530

    Create task2.js

---
+++ javaScript/ASSIGNMENT_8/task2.js
@@ -0,0 +1,13 @@
+// 2. Write the syntax for
+// a) Creating new set object
+// b) Adding new element to set object
+// c) Deleting element from set object
+
+// Create a new set object
+const set = new Set();
+//add the value 1
+set.add(1);
+// Delete the value 3 from the set
+set.delete(3);
+
+
