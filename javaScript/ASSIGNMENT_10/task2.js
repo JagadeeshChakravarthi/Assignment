83e5a4f6b28f3ea685fbd11feaddcaab8acb7ee6
JagadeeshChakravarthi <110399596+JagadeeshChakravarthi@users.noreply.github.com>
2023-06-07 10:34:38 +0530

    Create task2.js

---
+++ javaScript/ASSIGNMENT_10/task2.js
@@ -0,0 +1,5 @@
+Is filter a Higher Order function in Javascript ? If yes, why ?
+
+Yes, filter() is a higher-order function in JavaScript. 
+A higher-order function is a function that takes one or more functions as arguments, or returns a function as its result. 
+The filter() function takes a function as an argument,and then returns a new array containing only the elements of the original array that pass the test specified by the function.
