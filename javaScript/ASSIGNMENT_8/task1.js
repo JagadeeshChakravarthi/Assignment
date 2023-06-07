c843c5834ee30cd79abe2535945db122f802715c
JagadeeshChakravarthi <110399596+JagadeeshChakravarthi@users.noreply.github.com>
2023-06-07 09:49:04 +0530

    Create task1.js

---
+++ javaScript/ASSIGNMENT_8/task1.js
@@ -0,0 +1,5 @@
+1. Can we put duplicate values in the set object ?
+  
+No, you cannot put duplicate values in a set object in JavaScript.
+A set is a collection that only contains unique values. 
+If you try to add a duplicate value to a set, it will simply be ignored.
